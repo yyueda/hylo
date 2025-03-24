5b91c911fcdac3297daddfcb287bda30799e7fdf
yyueda <yyuedaa@gmail.com>
2025-03-24 09:35:06 +0800

    Add clerk middleware

---
+++ src/app/(auth)/onboarding/page.tsx
@@ -0,0 +1,9 @@
+async function Page() {
+    return (
+        <main>
+            <h1 className="">Onboarding</h1>
+        </main>
+    );
+}
+
+export default Page;
--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,4 +1,12 @@
 import type { Metadata } from "next";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
 import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
 
@@ -23,12 +31,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+      <ClerkProvider>
+        <html lang="en">
+          <body
+            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          >
+            <header className="flex justify-end items-center p-4 gap-4 h-16">
+              <SignedOut>
+                <SignInButton />
+                <SignUpButton />
+              </SignedOut>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </header>
+            {children}
+          </body>
+        </html>
+      </ClerkProvider>
   );
 }
