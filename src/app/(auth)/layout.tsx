ca565b87420e2f03cc55bc7b533d0339a99208a7
yyueda <yyuedaa@gmail.com>
2025-03-25 12:42:25 +0800

    Add top bar

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,8 +1,9 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
 import "../globals.css";
+import { Metadata } from "next";
 
-export const metadata = {
+export const metadata: Metadata = {
     title: 'Threads',
     description: 'A NextJS Threads Application'
 }
