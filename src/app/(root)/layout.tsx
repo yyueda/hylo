d360a8a2ca6aa70056be643e3a156bf9dda04132
yyueda <yyuedaa@gmail.com>
2025-03-26 15:10:40 +0800

    Add right sidebar

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
             className={`${geistSans.variable} ${geistMono.variable} antialiased`}
           >
             <Topbar />
-              <main>
+              <main className="flex">
                 <LeftSidebar />
                   <section className="main-container">
                     <div className="w-full max-w-4xl">
