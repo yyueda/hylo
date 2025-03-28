04c79276edf14effc231bcddd36ded42065b2e74
yyueda <yyuedaa@gmail.com>
2025-03-28 19:51:55 +0800

    Allow file uploads to uploadthing

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  serverExternalPackages: ['mongoose'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com',
+        port: '',
+        search: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
