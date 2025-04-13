b873a1e4f17c87b1abf37910bce9b29b3c5b1f71
yyueda <yyuedaa@gmail.com>
2025-04-13 14:35:58 +0800

    Ignore eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // Disable specific TypeScript rules
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+    },
+  }
 ];
 
 export default eslintConfig;
